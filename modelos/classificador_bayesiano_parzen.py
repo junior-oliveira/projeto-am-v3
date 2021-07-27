166c7b29e7e8d78df1adc6e5d1c04db3a0df08e5
junior.ufal@gmail.com <junior.ufal@gmail.com>
2021-07-27 02:36:17 -0300

    Alguns ajustes, comentários e limpeza do código

--- modelos/classificador_bayesiano_parzen.py
+++ modelos/classificador_bayesiano_parzen.py
@@ -106,7 +106,6 @@ class ClassificadorBayesianoParzen(BaseEstimator, ClassifierMixin):
         
         
         #Estimação do vetor de médias
-        
         for k in range(len(self.classes_)):                         
             for i in range(len(y)):
                 if(self.classes_[k] == y[i]):                 
@@ -120,107 +119,82 @@ class ClassificadorBayesianoParzen(BaseEstimator, ClassifierMixin):
                     self.variancias_[k] += np.linalg.norm(np.subtract(X[i],self.mu_[k]))**2    
             self.variancias_[k] = self.variancias_[k]/(self.n_de_exemplos_classes_[k]*self.n_atributos_)
         
-        #for xk in X:
-        #    for k in range(len(self.classes_)):
-        #        self.variancias_[k] += np.linalg.norm(np.subtract(xk,self.mu_[k]))**2    
-        #self.variancias_ = self.variancias_/(self.numero_de_exemplos_*self.n_atributos_)
-        
+  
         # Estimação das matrizes sigma e sigma_inv
         for k in range(len(self.classes_)):
             np.fill_diagonal(self.sigma_[k], self.variancias_[k])
             self.sigma_inv_[k] = np.linalg.inv(self.sigma_[k])
-        
-    # Calcula a norma conforme especificado no trablho. Verificar se o resultado melhora em relação a np.linalg.norm
-    def norma(self, vetor):    
-        soma = 0.0    
-        for elem in vetor:
-            soma += elem
-        return soma
     
     def pxk_wi(self, xk, wi, y):
+        """ 
+        Parâmetros:
+            xk - k-ézimo exemplo da base de treinamento para o qual se deseja estimar a densidade
+            wi - classe na qual deve ser estimada a densidade de xk
+            y - rótulos de todas as classes do conjunto de treinamento
+
+        Retorno:
+            Densidade estimada para xk através da janela de parzen
+        """
+        # Armazena todos os exemplos do conjunto de treinamento, pertencentes à classe wi
         exemplos_wi = self.X_[y == wi]
-        #t = time.time()
-        res = self.parzen_estimation(exemplos_wi, xk, self.h, self.dim, self.parzen_window_produto_multivariada, self.kernel_gaussiano_univariado)
-        #elapsed = time.time() - 1
-        #print('tempo gasto: ', elapsed) 
+        
+        # Retorna o valor estimado da densidade para o exemplo xk
+        res = self.est_densidade_janela_de_parzen(exemplos_wi, xk, self.h, self.dim)
+        
         return res
 
-    def posteriores(self, verossimilhancas):
+    def posteriores(self, verossimilhancas_x):
         """ 
-        verossimilhancas - array unidimensional contendo as verossimilhanças para todas as classes
+        Parâmetros:
+            verossimilhancas_x - array unidimensional contendo as verossimilhanças para todas as classes 
+            dado um ponto x
+
+        Retorno:
+            Array contendo as probabilidades a posteriori estimada para todas as classes de um ponto x
         """
-        posteriores = np.multiply(verossimilhancas, self.probabilidades_a_priori) / np.dot(verossimilhancas, self.probabilidades_a_priori)
+        posteriores = np.multiply(verossimilhancas_x, self.probabilidades_a_priori) / np.dot(verossimilhancas_x, self.probabilidades_a_priori)
 
         return posteriores
     
-    def hypercube_kernel(self, h, x, x_i, d):
-
-        """
-        Implementation of a hypercube kernel for Parzen-window estimation.
-
-        Keyword arguments:
-            h: window width
-            x: point x for density estimation, 'd x 1'-dimensional numpy array
-            x_i: point from training sample, 'd x 1'-dimensional numpy array
-
-        Returns a 'd x 1'-dimensional numpy array as input for a window function.
-
-        """
-        assert (x.shape == x_i.shape), 'vectors x and x_i must have the same dimensions'
-        return (x - x_i) / (h)
-    
     def kernel_gaussiano_univariado(self, h, x, x_i, d):
+        """
+        Parâmetros:
+            h: tamanho da janela
+            x: ponto x para estimação de densidade (array numpy 'd x 1')
+            x_i: exemplo do conjunto de treinamento (array numpy 'd x 1')
         
+        Retorno:
+            Array 'd x 1' com todos os valores da norma univariada  
+        """
         
-        assert (x.shape == x_i.shape), 'vectors x and x_i must have the same dimensions'
+        # Verifica se os vetores possuem as mesmas dimensões
+        assert (x.shape == x_i.shape), 'vetores x and x_i devem ter as mesmas dimensões'
 
-        res = (1/(np.sqrt(2*np.pi))) * np.exp(-(((x - x_i) / h)**2) /2)
-        return res
+        # Calcula a normal univariada para cada uma das d dimensões (d é o tamaho do vetor 'd x 1')
+        normas_univariadas = (1/(np.sqrt(2*np.pi))) * np.exp(-(((x - x_i) / h)**2) /2)
+
+        # Calcula o kernel multivariado do produto a partir do vetor de normas univariadas. 
+        # Uma vez que as normas já foram calculadas, basta calcular o produtório
+        kernel_multiv_prod = np.prod(normas_univariadas)
+        return kernel_multiv_prod
 
 
-    def parzen_window_func(self, x_vec, h=1):
+    def est_densidade_janela_de_parzen(self, exemplos, ponto_x, h, d):
         """
-        Implementation of the window function. Returns 1 if 'd x 1'-sample vector
-        lies within inside the window, 0 otherwise.
+        Estimador Janela de Parzen
 
-        """
-        for row in x_vec:
-            if np.abs(row) > (1/2):
-                return 0
-        return 1
+        Parâmetros:
+            exemplos: array numpy 'n x d'-dimensional, cada amostra armazenada em uma linha separada
+            ponto_x: ponto x para estimação de densidade, array numpy 'd x 1'-dimensional
+            h: largura da janela
+            d: dimensões (quantidade de atributos)
 
-    def parzen_window_produto_multivariada(self, x_vec, h=1):
-        """
-        Implementation of the window function. Returns 1 if 'd x 1'-sample vector
-        lies within inside the window, 0 otherwise.
-
-        """
-        return np.prod(x_vec)
-        for row in x_vec:
-            if np.prod(row) > (1/2):
-                return 0
-        return 1
-        
-
-    def parzen_estimation(self, x_samples, point_x, h, d, window_func, kernel_func):
-        """
-        Implementation of a parzen-window estimation.
-
-        Keyword arguments:
-            x_samples: A 'n x d'-dimensional numpy array, where each sample
-                is stored in a separate row. (= training sample)
-            point_x: point x for density estimation, 'd x 1'-dimensional numpy array
-            h: window width
-            d: dimensions
-            window_func: a Parzen window function (phi)
-            kernel_function: A hypercube or Gaussian kernel functions
-
-        Returns the density estimate p(x).
+        Retorno:
+            Desidade estimada p(x) referente ao ponto x
 
         """
         #k_n representa o número de exemplos dentro da região
         k_n = 0
-        for row in x_samples:
-            x_i = kernel_func(h=h, x=point_x, x_i=row, d=d)
-            k_n += window_func(x_i, h=h)
-        return (k_n / len(x_samples)) / (h**d)
+        for row in exemplos:
+            k_n += self.kernel_gaussiano_univariado(h=h, x=ponto_x, x_i=row, d=d) 
+        return (k_n / len(exemplos)) / (h**d)
